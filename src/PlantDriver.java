45579b7f7b8651f9ccb8e7ba2563b92e23c21620
LauraDelage <laura.delage@ashbury.ca>
2022-04-19 14:42:36 -0400

    Adding commenting

--- src/PlantDriver.java
+++ src/PlantDriver.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-//hello
-//helloo
+/**
+ * This is the PlantDriver Class
+ * This class controls the GUI and facilitates user input and output
+ * @author: Keelin Saranchuk
+ * */
 
 public class PlantDriver extends Application {
     @Override
