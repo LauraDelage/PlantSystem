b41ed654d989687f1be076bb0ee5cd6ef4f14b80
keelinsaranchuk <62619328+keelinsaranchuk@users.noreply.github.com>
2022-04-21 21:02:59 -0400

    adding title image

--- src/PlantDriver.java
+++ src/PlantDriver.java
@@ -1,4 +1,9 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -8,12 +13,29 @@ import javafx.stage.Stage;
  * */
 
 public class PlantDriver extends Application {
+    Pane homeCanvas = new Pane();
+    ImageView startImage = new ImageView();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Scene homeScene = new Scene(homeCanvas, 600, 780, Color.WHITE);
+        primaryStage.setScene(homeScene);
+        primaryStage.show();
+        startHomeScene();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void startHomeScene() {
+        //Defining main title image:
+        startImage.setImage(new Image("plantTitle.jpg"));
+        startImage.setX(0);
+        startImage.setY(-20);
+        startImage.setFitWidth(600);
+        startImage.setPreserveRatio(true);
+        //Adding all elements to the canvas:
+        homeCanvas.getChildren().add(startImage);
     }
 }
