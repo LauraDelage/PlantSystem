de54fe19b1ab0baa032d43c69c2478fe3e2c2a25
keelinsaranchuk <62619328+keelinsaranchuk@users.noreply.github.com>
2022-04-24 15:00:12 -0400

    button

--- src/PlantDriver.java
+++ src/PlantDriver.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -15,6 +17,7 @@ import javafx.stage.Stage;
 public class PlantDriver extends Application {
     Pane homeCanvas = new Pane();
     ImageView startImage = new ImageView();
+    Button addPlant = new Button("Add Plant");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -29,13 +32,22 @@ public class PlantDriver extends Application {
     }
 
     public void startHomeScene() {
+        homeCanvas.setStyle("-fx-background-color: white;");
+        Font font2 = new Font("Impact", 20); //Impact
         //Defining main title image:
         startImage.setImage(new Image("plantTitle.jpg"));
         startImage.setX(0);
         startImage.setY(-20);
         startImage.setFitWidth(600);
         startImage.setPreserveRatio(true);
+        //Defining add plant button:
+        addPlant.setStyle("-fx-background-color: #488940");
+        addPlant.setLayoutX(30);
+        addPlant.setLayoutY(710);
+        addPlant.setTextFill(Color.WHITE);
+        addPlant.setFont(font2);
         //Adding all elements to the canvas:
         homeCanvas.getChildren().add(startImage);
+        homeCanvas.getChildren().add(addPlant);
     }
 }
