ea9a8c0a4bb82f1e1f7a77ba51b4df046844e9f2
LauraDelage <laura.delage@ashbury.ca>
2022-04-27 11:39:37 -0400

    Added parameters and getter methods in the Plant class and created the PlantSorter method.

--- src/Plant.java
+++ src/Plant.java
@@ -1,2 +1,22 @@
+/**
+ * Plant class
+ * Creates the structure for an individual plant object.
+ * @author: Keelin Saranchuk, Laura Delage
+ * */
+
 public class Plant {
+    String plantName;
+    String plantSpecies;
+    String plantType;
+    int waterReq;
+
+    public String getName() {
+        return plantName;
+    }
+
+    public int getWaterReq() {
+        return waterReq;
+    }
+
+
 }
